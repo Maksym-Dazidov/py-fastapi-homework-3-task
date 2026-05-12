16fa24f5e82d256a610cb11a59b32f8f801e80dc
Maksym Dazidov <maksymdazidov@gmail.com>
2026-05-12 23:47:35 +0200

    solution

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -35,18 +35,20 @@ from src.security.utils import generate_secure_token
 
 router = APIRouter()
 
+
 @router.post("/register/")
 async def register(user: UserRegistrationSchema, db: AsyncSession = Depends(get_db)):
     db_user = await db.scalar(select(UserModel).where(UserModel.email == user.email))
     if db_user:
-        raise HTTPException(status_code=400, detail="User already exists")
+        raise HTTPException(status_code=409, detail=f"A user with this email {user.email} already exists.")
     new_user = UserModel(email=user.email, password=hash_password(user.password))
     db.add(new_user)
     await db.commit()
-    return new_user
+    return {"id": f"{new_user.id}", "email": f"{new_user.email}"}
+
 
 @router.post("/activate/")
-async def activate(user: UserRegistrationSchema, db: AsyncSession = Depends(get_db)):
+async def activate(user: UserActivationSchema, db: AsyncSession = Depends(get_db)):
     db_user = await db.scalar(select(UserModel).where(UserModel.email == user.email))
     if user.activation_token:
         db_user.is_active = True
@@ -54,19 +56,19 @@ async def activate(user: UserRegistrationSchema, db: AsyncSession = Depends(get_
     await db.delete(user.activation_token)
     await db.commit()
 
-    return {"status": "successfully activated"}
+    return {"message": "User account activated successfully."}
 
 
 @router.post("/password-reset/request/")
 async def request_password_reset_token(user: UserBaseSchema, db: AsyncSession = Depends(get_db)):
     db_user = await db.scalar(select(UserModel).where(UserModel.email == user.email))
-    if not db_user or not user.is_active:
+    if not db_user or not db_user.is_active:
         return MessageResponseSchema(message="If you are registered, you will receive an email with instructions.")
     if user.password_reset_token:
         await db.delete(user.password_reset_token)
-    user.password_reset_token = UserPasswordResetTokenModel(
+    user.password_reset_token = PasswordResetTokenModel(
         token=generate_secure_token(),
-        user_id=user.id
+        user_id=db_user.id
     )
     await db.commit()
     return MessageResponseSchema(message="If you are registered, you will receive an email with instructions.")
@@ -76,7 +78,7 @@ async def request_password_reset_token(user: UserBaseSchema, db: AsyncSession = 
 async def reset_password_complete(user: PasswordResetCompleteSchema, db: AsyncSession = Depends(get_db)):
     db_user = await db.scalar(select(UserModel).where(UserModel.email == user.email))
     if not db_user:
-        raise HTTPException(status_code=400, detail="User does not exist")
+        raise HTTPException(status_code=400, detail="Invalid email or token.")
     if not user or not user.password_reset_token:
         raise HTTPException(status_code=400, detail="Invalid email or token.")
     try:
@@ -88,15 +90,17 @@ async def reset_password_complete(user: PasswordResetCompleteSchema, db: AsyncSe
         db.rollback()
         raise HTTPException(status_code=500, detail="An error occurred while resetting the password.")
 
+
 @router.post("/login/")
-async def login(user: UserRegistrationSchema, db: AsyncSession = Depends(get_db), jwt_manager: JWTAuthManagerInterface = Depends(get_jwt_auth_manager)):
+async def login(user: UserRegistrationSchema, db: AsyncSession = Depends(get_db),
+                jwt_manager: JWTAuthManagerInterface = Depends(get_jwt_auth_manager)):
     db_user = await db.scalar(select(UserModel).where(UserModel.email == user.email))
-    if not db_user:
-        raise HTTPException(status_code=400, detail="User does not exist")
+    if not db_user or not db_user.is_active or not verify_password(login_data.password, user.hashed_password):
+        raise HTTPException(status_code=400, detail="Invalid email or token.")
     try:
         access_token = jwt_manager.create_access_token({"user_id": db_user.id})
         refresh_token = jwt_manager.create_refresh_token({"user_id": db_user.id})
-        db_refresh_token = RefreshTokenModel(refresh_token=refresh_token, user_id=db_user.id, days_valid=7)
+        db_refresh_token = RefreshTokenModel(token=refresh_token, user_id=db_user.id, days_valid=7)
         db.add(db_refresh_token)
         await db.flush()
         await db.commit()
@@ -109,10 +113,13 @@ async def login(user: UserRegistrationSchema, db: AsyncSession = Depends(get_db)
         await db.rollback()
         raise HTTPException(status_code=500, detail="An error occurred while processing the request.")
 
-@router.post("/refresh/")
-async def refresh_account_token(refresh_token: TokenRefreshRequestSchema, jwt_manager = Depends(get_jwt_auth_manager), db: AsyncSession = Depends(get_db)):
+
+@router.post("/api/v1/accounts/refresh/")
+async def refresh_account_token(refresh_token: TokenRefreshRequestSchema,
+                                jwt_manager: JWTAuthManagerInterface = Depends(get_jwt_auth_manager),
+                                db: AsyncSession = Depends(get_db)):
     db_token = await db.scalar(
-        select(RefreshTokenModel).where(RefreshTokenModel.token == refresh_token_data.refresh_token)
+        select(RefreshTokenModel).where(RefreshTokenModel.token == refresh_token.refresh_token)
     )
     if not db_token:
         raise HTTPException(status_code=401, detail="Refresh token not found.")
--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -6,22 +6,29 @@ from database import accounts_validators
 class UserBaseSchema(BaseModel):
     email: EmailStr
 
+
 class UserRegistrationSchema(UserBaseSchema):
     password: str
+
 
 class UserActivationSchema(UserBaseSchema):
     token: str
 
+
 class MessageResponseSchema(BaseModel):
     message: str
+
 
 class PasswordResetCompleteSchema(UserBaseSchema):
     token: str
     password: str
+
+
 class UserLoginResponseSchema(BaseModel):
     access_token: str
     refresh_token: str
     token_type: str
+
 
 class TokenRefreshRequestSchema(BaseModel):
     refresh_token: str
